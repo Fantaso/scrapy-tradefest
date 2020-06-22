92bcbca11cbc150b402a43f7a617323eb16bbaa9
Carlos Rosas <fantaso.code@gmail.com>
2020-06-22 17:34:07 +0200

    remove unnecessary imports and comments

--- tradefest_scraper/tradefest_scraper/settings.py
+++ tradefest_scraper/tradefest_scraper/settings.py
@@ -1,16 +1,5 @@
 # -*- coding: utf-8 -*-
-
-# Scrapy settings for tradefest_scraper project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-import logging
 import pathlib
-import time
 from datetime import datetime
 
 BOT_NAME = "tradefest_scraper"
