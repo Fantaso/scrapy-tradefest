88ec36e68b90a352846b97fe4daaf3898f948255
Carlos Rosas <fantaso.code@gmail.com>
2020-06-23 21:03:34 +0200

    Now the scraper can download images from each furniture event. added the custom image pipeline to get and set the name of the image for every request to download an image

--- tradefest_scraper/tradefest_scraper/pipelines.py
+++ tradefest_scraper/tradefest_scraper/pipelines.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
 
 
-class TradefestScraperPipeline:
-    def process_item(self, item, spider):
-        return item
+class TradefestImagesPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        """
+        Getting the custom name for the image file.
+        """
+        image_name = {'image_name': item.get('image_name')}
+        media_requests = [
+            Request(x, meta=image_name)
+            for x in item.get(self.images_urls_field, [])
+        ]
+        return media_requests
+
+    def file_path(self, request, response=None, info=None):
+        """
+        Setting the custom name for the image file.
+        """
+        image_guid = request.meta.get('image_name')
+        return 'images/%s.jpg' % (image_guid)
